b5cfc7eae8ef3cb7adf30f6b8a109e5ed15fd4d3
Laure Juglaret <ljuglaret@gmail.com>
2021-04-09 17:56:25 +0200

    images

--- src/app/qcm-form/qcm-form.component.ts
+++ src/app/qcm-form/qcm-form.component.ts
@@ -11,24 +11,29 @@ import data from '../donnees/qcm.json';
   <div class="container">
   <div [hidden]="submitted">
   <form (ngSubmit)="onSubmit()" #qcmForm="ngForm">
-   <div *ngFor="let liste of this.qcminitial; let in=index" >
-        <div class="form-group">        
+  <div *ngFor="let liste of this.qcminitial; let in=index ; " >
+        <div ngClass="box" class="form-group">        
+          <div > 
+            <img [src] = listeImages[in] />
+          </div>
+          <div>
             <h2 >{{this.qcminitial[in].intituleQuestion}}</h2>
             
             <div><label for="choix1Q{{in}}">{{this.qcminitial[in].choix[0]}}</label>
             <input type="radio" id="choix1Q{{in}}" name="question{{in}}" value=0 [(ngModel)]="this.qcminitial[in].repFournie"></div>
-            
+              
             <div><label for="choix2">{{this.qcminitial[in].choix[1]}}</label>
             <input type="radio" id="choix1Q{{in}}" name="question{{in}}" value= 1 [(ngModel)]="this.qcminitial[in].repFournie"/></div>
-            
+              
             <div><label for="choix3">{{this.qcminitial[in].choix[2]}}</label>
             <input type="radio" id="choix1Q{{in}}" name="question{{in}}" value= 2 [(ngModel)]="this.qcminitial[in].repFournie"/></div>
-            
+              
             <div><label for="choix4">{{this.qcminitial[in].choix[3]}}</label>
             <input type="radio" id="choix1Q{{in}}" name="question{{in}}" value= 3 [(ngModel)]="this.qcminitial[in].repFournie"/></div>
 
+          </div>
         </div>
-  </div>
+    </div>
   <nav>
   <a (click)="submitBtn()" routerLink="/qcm-resultat" routerLinkActive="active">Correction</a>
 </nav>   
@@ -45,9 +50,13 @@ export class qcmFormComponent {
   
   model =  new QCM();
 
+  listeImages = [
+    "https://static.teteamodeler.com/media/cache/thumb_400/elephant-explication-tete-a-modeler-du-mot-elephant.png",
+    "https://www.mexique-voyages.com/wp-content/uploads/adobestock-293077837-1280x800.jpeg",
+    "https://images-na.ssl-images-amazon.com/images/I/71XkzlbxEbL._AC_SL1001_.jpg",
+    "https://cdn.radiofrance.fr/s3/cruiser-production/2016/08/be123547-b500-4b59-aeb3-d0e9a85fa1cc/838_f-g_grandin_fgg_4533.jpg"
+  ]
   submitted = false;
-
-  
   onSubmit() { this.submitted = true; 
   
   }
